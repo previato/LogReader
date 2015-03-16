adf580139c8b7541a5c40552092f5dbba5917843
beaguinho <rpreviato@outlook.com>
2015-03-16 14:05:43 +0000

    Start project

---
+++ LogReader/FormMain.Designer.cs
@@ -0,0 +1,199 @@
+﻿namespace LogReader
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStartLog = new System.Windows.Forms.Button();
+            this.txtPath = new System.Windows.Forms.TextBox();
+            this.txtLog = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnBrowserFolder = new System.Windows.Forms.Button();
+            this.btnStopLog = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.upClearLineCount = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.chkClearWhenNewFile = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.upClearLineCount)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnStartLog
+            // 
+            this.btnStartLog.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnStartLog.Location = new System.Drawing.Point(638, 10);
+            this.btnStartLog.Name = "btnStartLog";
+            this.btnStartLog.Size = new System.Drawing.Size(75, 48);
+            this.btnStartLog.TabIndex = 0;
+            this.btnStartLog.Text = "Iniciar";
+            this.btnStartLog.UseVisualStyleBackColor = true;
+            this.btnStartLog.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtPath
+            // 
+            this.txtPath.Location = new System.Drawing.Point(92, 12);
+            this.txtPath.Name = "txtPath";
+            this.txtPath.Size = new System.Drawing.Size(377, 20);
+            this.txtPath.TabIndex = 1;
+            // 
+            // txtLog
+            // 
+            this.txtLog.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtLog.BackColor = System.Drawing.Color.Black;
+            this.txtLog.Font = new System.Drawing.Font("MS Reference Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtLog.ForeColor = System.Drawing.Color.Lime;
+            this.txtLog.Location = new System.Drawing.Point(12, 64);
+            this.txtLog.Multiline = true;
+            this.txtLog.Name = "txtLog";
+            this.txtLog.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtLog.Size = new System.Drawing.Size(782, 496);
+            this.txtLog.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Caminho:";
+            // 
+            // btnBrowserFolder
+            // 
+            this.btnBrowserFolder.Location = new System.Drawing.Point(475, 10);
+            this.btnBrowserFolder.Name = "btnBrowserFolder";
+            this.btnBrowserFolder.Size = new System.Drawing.Size(75, 23);
+            this.btnBrowserFolder.TabIndex = 4;
+            this.btnBrowserFolder.Text = "Selecionar";
+            this.btnBrowserFolder.UseVisualStyleBackColor = true;
+            this.btnBrowserFolder.Click += new System.EventHandler(this.btnBrowserFolder_Click);
+            // 
+            // btnStopLog
+            // 
+            this.btnStopLog.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnStopLog.Enabled = false;
+            this.btnStopLog.Location = new System.Drawing.Point(719, 10);
+            this.btnStopLog.Name = "btnStopLog";
+            this.btnStopLog.Size = new System.Drawing.Size(75, 48);
+            this.btnStopLog.TabIndex = 5;
+            this.btnStopLog.Text = "Parar";
+            this.btnStopLog.UseVisualStyleBackColor = true;
+            this.btnStopLog.Click += new System.EventHandler(this.btnStopLog_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 40);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(74, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Limpar a cada";
+            // 
+            // upClearLineCount
+            // 
+            this.upClearLineCount.Increment = new decimal(new int[] {
+            500,
+            0,
+            0,
+            0});
+            this.upClearLineCount.Location = new System.Drawing.Point(92, 38);
+            this.upClearLineCount.Maximum = new decimal(new int[] {
+            100000,
+            0,
+            0,
+            0});
+            this.upClearLineCount.Name = "upClearLineCount";
+            this.upClearLineCount.Size = new System.Drawing.Size(87, 20);
+            this.upClearLineCount.TabIndex = 7;
+            this.upClearLineCount.Value = new decimal(new int[] {
+            3000,
+            0,
+            0,
+            0});
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(185, 40);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(34, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "linhas";
+            // 
+            // chkClearWhenNewFile
+            // 
+            this.chkClearWhenNewFile.AutoSize = true;
+            this.chkClearWhenNewFile.Checked = true;
+            this.chkClearWhenNewFile.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.chkClearWhenNewFile.Location = new System.Drawing.Point(254, 39);
+            this.chkClearWhenNewFile.Name = "chkClearWhenNewFile";
+            this.chkClearWhenNewFile.Size = new System.Drawing.Size(158, 17);
+            this.chkClearWhenNewFile.TabIndex = 9;
+            this.chkClearWhenNewFile.Text = "Limpar a cada novo arquivo";
+            this.chkClearWhenNewFile.UseVisualStyleBackColor = true;
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(806, 572);
+            this.Controls.Add(this.chkClearWhenNewFile);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.upClearLineCount);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnStopLog);
+            this.Controls.Add(this.btnBrowserFolder);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtLog);
+            this.Controls.Add(this.txtPath);
+            this.Controls.Add(this.btnStartLog);
+            this.Name = "FormMain";
+            this.Text = "Log Reader";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.FormMain_FormClosing);
+            this.Load += new System.EventHandler(this.FormMain_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.upClearLineCount)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStartLog;
+        private System.Windows.Forms.TextBox txtPath;
+        private System.Windows.Forms.TextBox txtLog;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnBrowserFolder;
+        private System.Windows.Forms.Button btnStopLog;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown upClearLineCount;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.CheckBox chkClearWhenNewFile;
+    }
+}
+
---
+++ LogReader/FormMain.cs
@@ -0,0 +1,271 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace LogReader
+{
+    public partial class FormMain : Form
+    {
+        private List<String> _threadList;
+        private String _currentThreadId;
+        private Boolean _cancellAll;
+        private Thread _logThread;
+
+        private Boolean _clearWhenNewFile;
+        private Int32 _clearLineCount;
+
+        public FormMain()
+        {
+            InitializeComponent();
+
+            _threadList = new List<string>();
+        }
+
+        private String GetMostRecentFile(String path)
+        {
+            var logFile = default(string);
+            var files = Directory.GetFiles(path);
+            var lastDate = default(DateTime);
+
+            // determinar o arquivo mais recente.
+            foreach (var file in files)
+            {
+                var fi = new FileInfo(file);
+                if (fi.LastWriteTime > lastDate)
+                {
+                    lastDate = fi.LastWriteTime;
+                    logFile = file;
+                }
+            }
+
+            return logFile;
+        }
+
+        private void ProcessLog(object args)
+        {
+            var hasNewFile = false;
+            string path = args.ToString();
+            string logFile = "";
+            string id = Thread.CurrentThread.Name;
+
+            var reset = new AutoResetEvent(false);
+
+            var fw = new FileSystemWatcher(path);
+            fw.Filter = "*.*";
+            fw.Changed += (s, e) =>
+            {
+                reset.Set();
+            };
+            fw.Created += (s, e) =>
+            {
+                hasNewFile = true;
+                reset.Set();
+            };
+            fw.EnableRaisingEvents = true;
+
+            var lineCount = 0;
+
+            Action<String> logAppend = (x) =>
+            {
+                if (lineCount >= _clearLineCount)
+                {
+                    this.txtLog.Text = string.Concat("==== LIMPANDO DEVIDO AO NÚMERO MÁXIMO DE LINHAS TER ATINGIDO ====", "\r\n");
+                    lineCount = 0;
+                }
+                this.txtLog.AppendText(string.Concat(x, "\r\n"));
+                lineCount++;
+            };
+
+            Action<String> logText = (x) =>
+            {
+                this.txtLog.Text = x;
+            };
+
+            try
+            {
+                while (true)
+                {
+                    logFile = this.GetMostRecentFile(path);
+                    if (string.IsNullOrEmpty(logFile))
+                    {
+                        this.Invoke(logAppend, "Não há arquivos no diretório. Nova tentativa em 2 segundos");
+                        Thread.Sleep(2000);
+                        continue;
+                    }
+                    do
+                    {
+                        hasNewFile = false;
+
+                        using (var fs = new FileStream(logFile, FileMode.Open, FileAccess.Read, FileShare.ReadWrite | FileShare.Delete))
+                        {
+                            using (var reader = new StreamReader(fs))
+                            {
+                                var firstRead = reader.ReadToEnd();
+
+                                if (_currentThreadId == id)
+                                {
+                                    this.Invoke(logAppend, firstRead.TrimEnd('\n').TrimEnd('\r'));
+                                }
+
+                                while (true)
+                                {
+                                    var line = default(string);
+                                    var log = "";
+
+                                    while ((line = reader.ReadLine()) != null)
+                                    {
+                                        log = string.Concat(log, line, "\r\n");
+                                    }
+                                    log = log.TrimEnd('\n').TrimEnd('\r');
+
+                                    if (_currentThreadId == id && !string.IsNullOrEmpty(log))
+                                    {
+                                        this.Invoke(logAppend, log);
+                                    }
+
+                                    reset.WaitOne(1000);
+
+                                    if (_cancellAll) break;
+
+                                    // verificar se há um arquivo mais recente.
+                                    if (hasNewFile)
+                                    {
+                                        logFile = this.GetMostRecentFile(path);
+                                        var act = default(Action<string>);
+                                        if (_clearWhenNewFile)
+                                        {
+                                            act = logText;
+                                        }
+                                        else
+                                        {
+                                            act = logAppend;
+                                        }
+                                        this.Invoke(act, string.Format("------------------- NOVO ARQUIVO: {0} -------------------", logFile));
+                                        break;
+                                    }
+                                }
+                            }
+                        }
+                    } while (hasNewFile);
+                }
+            }
+            catch (System.Threading.ThreadAbortException ex)
+            {
+                this.Invoke(logAppend, "Leitura de log parada");
+                lock (_threadList)
+                {
+                    _threadList.Remove(id);
+                }
+            }
+            catch (Exception ex)
+            {
+                this.Invoke(logAppend, ex.ToString());
+            }
+
+            lock (_threadList)
+            {
+                this.Invoke(logAppend, string.Format("Finalizando thread: {0}", id));
+                _threadList.Remove(id);
+            }
+        }
+
+        private void ProcessClose()
+        {
+            Action act = () =>
+            {
+                this.Close();
+            };
+            while (true)
+            {
+                lock (_threadList)
+                {
+                    if (_threadList.Count == 0)
+                    {
+                        this.Invoke(act);
+                        break;
+                    }
+                }
+                Thread.Sleep(50);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var threadId = Guid.NewGuid().ToString();
+
+            _logThread = new Thread(new ParameterizedThreadStart(this.ProcessLog));
+            _currentThreadId = _logThread.Name = threadId;
+            _threadList.Add(threadId);
+
+            _clearLineCount = Convert.ToInt32(this.upClearLineCount.Value);
+            _clearWhenNewFile = this.chkClearWhenNewFile.Checked;
+            this.txtLog.Clear();
+
+            _logThread.Start(this.txtPath.Text);
+
+            Properties.Settings.Default.LastPath = this.txtPath.Text;
+            Properties.Settings.Default.Save();
+
+            this.upClearLineCount.Enabled = false;
+            this.chkClearWhenNewFile.Enabled = false;
+            this.txtPath.Enabled = false;
+            this.btnBrowserFolder.Enabled = false;
+            this.btnStartLog.Enabled = false;
+            this.btnStopLog.Enabled = true;
+        }
+
+        private void FormMain_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (_threadList.Count > 0)
+            {
+                System.Diagnostics.Debug.WriteLine("Aguardando cancelamento das Threads");
+                _cancellAll = true;
+                e.Cancel = true;
+
+                var t = new Thread(new ThreadStart(this.ProcessClose));
+                t.Start();
+            }
+        }
+
+        private void btnStopLog_Click(object sender, EventArgs e)
+        {
+            _logThread.Abort();
+            this.btnStopLog.Enabled = false;
+
+            this.upClearLineCount.Enabled = true;
+            this.chkClearWhenNewFile.Enabled = true;
+            this.txtPath.Enabled = true;
+            this.btnStartLog.Enabled = true;
+            this.btnBrowserFolder.Enabled = true;
+        }
+
+        private void btnBrowserFolder_Click(object sender, EventArgs e)
+        {
+            using (var f = new FolderBrowserDialog() { SelectedPath = this.txtPath.Text })
+            {
+                if (f.ShowDialog(this) == System.Windows.Forms.DialogResult.OK)
+                {
+                    this.txtPath.Text = f.SelectedPath.TrimEnd('\\') + "\\";
+                }
+            }
+        }
+
+        private void FormMain_Load(object sender, EventArgs e)
+        {
+            var lastPath = Properties.Settings.Default.LastPath;
+            if (!string.IsNullOrEmpty(lastPath))
+            {
+                this.txtPath.Text = lastPath;
+            }
+        }
+    }
+}
